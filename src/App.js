ca01328feb70ca46c085740e673c664ebd8c0698
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-10-12 15:30:08 -0700

    Added navbar, sidebar, signup form

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import Create from './pages/create/Create'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/Signup'
 import Project from './pages/project/Project'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
+          <Navbar />
           <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/create" element={<Create />} />
