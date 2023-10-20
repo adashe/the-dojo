feb8e41038d23f43a2e20ce535c6200a02386df7
Andrea Dashe <andrea.r.dashe@gmail.com>
2023-10-20 12:46:08 -0700

    Fetch and display messages from Firebase

--- src/pages/chat/Chat.js
+++ src/pages/chat/Chat.js
@@ -1,30 +1,60 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useCollection } from '../../hooks/useCollection'
 import { useDocument } from '../../hooks/useDocument'
 
 // styles
 import './Chat.css'
-import { useState } from 'react'
 
 export default function Chat() {
-  const { id } = useParams()
-  const { error, document: chatTo } = useDocument('users', id)
+  const { messagesError, documents: messages } = useCollection('messages')
+  const { id: recipientId } = useParams()
+  const { recipientError, document: recipient } = useDocument('users', recipientId)
   const [newMessage, setNewMessage] = useState('')
 
-  if (error) {
-    return <div className="error">{error}</div>
-  }
-  if (!chatTo) {
-    return <div className="loading">Loading...</div>
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(newMessage)
   }
 
+  if (recipientError) {
+    return <div className="error">{recipientError}</div>
+  }
+  if (!recipient) {
+    return <div className="loading">Loading...</div>
+  }
+
+  if (messagesError) {
+    return <div className="error">{messagesError}</div>
+  }
+  if (!recipient) {
+    return <div className="loading">Loading...</div>
+  }
+
+  console.log(messages)
+
   return (
     <div>
-      <h2 className="page-title">Chat with {chatTo.displayName}</h2>
+      <h2 className="page-title">Chat with {recipient.displayName}</h2>
+
+      <div className="message-list">
+        <ul>
+          {!messages && <p>No messages yet!</p>}
+          {messages && messages.map(message => (
+            <li key={message.id}>
+              <div className="message-author">
+                <p>from {message.fromUserDisplayName} to {message.toUserDisplayName}</p>
+              </div>
+              <div className="message-content">
+                <p>{message.content}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+
       <form className="send-message" onSubmit={handleSubmit}>
         <label>
           <span>Send a message:</span>
